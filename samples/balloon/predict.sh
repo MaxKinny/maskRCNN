dc0205c800701a7ef4e6e4d14f88b34759bc68cd
maxgundam <maxgundam@hotmail.com>
2019-10-10 00:37:33 +0800

    create repo

---
+++ samples/balloon/predict.sh
@@ -0,0 +1,2 @@
+python3 balloon.py detect --weights=../../logs/balloon20191009T1153/mask_rcnn_balloon_0001.h5  --dataset=../../data/balloon --subset='val'
+
---
+++ samples/balloon/train.sh
@@ -0,0 +1,2 @@
+python3 balloon.py train --dataset=../../data/balloon --weights=../../resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5
+
